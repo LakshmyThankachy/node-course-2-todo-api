ca6e3aae18039b6554174ee729299bf71ca43f6a
PRASANTH\prasanth <prasanth@zealogics.com>
2018-11-20 17:41:18 +0530

    modified for get todos app

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post('/todos',(req,res)=>{
     res.status(400).send(er)
   })
 })
+
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send(todos)
+  },(e)=>{
+    res.status(400).send(e)
+  })
+})
 app.listen(3000,()=>{
   console.log('Listening to 3000')
 })
